dd597c46a9137216ed86d5c84c6a9658d95453db
Jixun Wu <jixun.moe@gmail.com>
2021-12-12 18:41:17 +0000

    refactor: remove unused files

--- QMC2-decoder/crypto/IStreamEncryptAndDecrypt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-class IStreamEncryptAndDecrypt {
-	virtual void Decrypt(uint64_t offset, uint8_t* buf, size_t len) = 0;
-	virtual void Encrypt(uint64_t offset, uint8_t* buf, size_t len) = 0;
-};
